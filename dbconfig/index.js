429e069acf8c9f380c091bed7c6132f6ca440efd
vishnumoger <vishnumoger@gmail.com>
2024-01-28 10:39:24 +0530

    postgre sql conf

--- dbconfig/index.js
+++ dbconfig/index.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'postgres',
-    host: '34.100.209.48',
+    host: '34.126.221.176',
     database: 'airport-iot-db',
     password: 'gmr-infiquity',
     port: '5432'
